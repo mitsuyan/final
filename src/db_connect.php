78dd494a4eaea2d68f6fcdd7955e5973a6548cc5
mitsuyan <dabido.mitsu@icloud.com>
2024-01-10 10:07:19 +0900

    newfillcreate

---
+++ src/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517366-final';
+    const USER = 'LAA1517366';
+    const PASS = 'Pass1117';
+
+    $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+?>
